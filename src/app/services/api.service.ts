356690ddab7262ff40fed0c556abf895477cf1aa
Tobias Palmer <tobiaspalmer93@gmail.com>
2019-01-03 22:30:35 +0100

    fix rpc url

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -5,7 +5,7 @@ import {NodeService} from "./node.service";
 @Injectable()
 export class ApiService {
 
-  rpcUrl = `https://api.vault.nanonode.ninja/api/node-api`;
+  rpcUrl = `https://vault-api.mynano.ninja/api/node-api`;
   // rpcUrl = `http://localhost:9950/api/node-api`;
 
   constructor(private http: HttpClient, private node: NodeService) { }
